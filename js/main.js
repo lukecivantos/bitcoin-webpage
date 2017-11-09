737b7326ce3035d7e5270c6ad81dd3055e042509
Luke Civantos <lfcivantos@gmail.com>
2017-11-09 15:47:46 -0500

    Added Typed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 
 loadData();
 
+var firstSentence = "In January 2009, history was made when the mysterious Satoshi Nakamoto released the first software program implementing Bitcoin."
+var typed = new Typed('#top-text', {
+    strings: [firstSentence, "We can add more if we want"],
+    typeSpeed: 30
+});
+
 
 function loadData() {
 
