cbd57e048bf9432326136a75d7c362dfe97c5018
Luke Civantos <lfcivantos@gmail.com>
2017-11-10 16:05:43 -0500

    Added Map Data

--- js/main.js
+++ js/main.js
@@ -6,36 +6,3 @@ var typed = new Typed('#top-text', {
     strings: [firstSentence, "Enter to learn more."],
     typeSpeed: 30
 });
-
- $.ajax({
-        url: 'https://coinmap.org/api/v1/venues/',
-        dataType: 'jsonp',
-        success: function(data) {
-            console.log(data);
-        }
-    });
-
-
-/*
-$(document).ready(function() {
-    $.ajax({
-        url: "https://coinmap.org/api/v1/venues/"
-    }).then(function(data) {
-        console.log(data);
-    });
-});
-*/
-/*
-function loadData() {
-    // Proxy url
-    var proxy = 'http://michaeloppermann.com/proxy.php?format=xml&url=';
-    var url =  'https://coinmap.org/api/v1/venues/';
-
-    $.getJSON(url, function (json) {
-
-        // Set the variables from the results array
-        console.log(json);
-    });
-
-
-}*/
