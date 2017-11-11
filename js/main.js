f2bf7af0db0f97d9a1e603e045e8e66c68783b70
Luke Civantos <lfcivantos@gmail.com>
2017-11-11 13:31:57 -0500

    added Map

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@ var typed = new Typed('#top-text', {
     strings: [firstSentence, "Enter to learn more."],
     typeSpeed: 30
 });
+
+
+var url =  'https://coinmap.org/api/v1/venues/';
+var proxyurl = "https://cors-anywhere.herokuapp.com/";
+var proxy = 'http://michaeloppermann.com/proxy.php?format=xml&url=';
+
+
+jQuery.ajax({
+    url: proxyurl +url,
+    type: "GET",
+
+    contentType: 'application/json; charset=utf-8',
+    success: function(resultData) {
+        //here is your json.
+        // process it
+        console.log(resultData);
+        StationMap("vendorMap", resultData,[42.360082, -71.058880]);
+
+
+
+
+    },
+    error : function(jqXHR, textStatus, errorThrown) {
+    },
+
+    timeout: 120000,
+});
--- js/map.js
+++ js/map.js
@@ -1,24 +1,82 @@
-var url =  'https://coinmap.org/api/v1/venues/';
-var proxyurl = "https://cors-anywhere.herokuapp.com/";
-var proxy = 'http://michaeloppermann.com/proxy.php?format=xml&url=';
 
 
-jQuery.ajax({
-    url: proxyurl +url,
-    type: "GET",
+/*
+ *  StationMap - Object constructor function
+ *  @param _parentElement   -- HTML element in which to draw the visualization
+ *  @param _data            -- Array with all stations of the bike-sharing network
+ */
 
-    contentType: 'application/json; charset=utf-8',
-    success: function(resultData) {
-        //here is your json.
-        // process it
-        console.log(resultData);
+StationMap = function(_parentElement, _data, _mapPosition) {
+
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.mapPosition = _mapPosition;
 
 
+    this.initVis();
+};
 
 
-    },
-    error : function(jqXHR, textStatus, errorThrown) {
-    },
+/*
+ *  Initialize station map
+ */
 
-    timeout: 120000,
-});
+StationMap.prototype.initVis = function() {
+    var vis = this;
+
+    // Instantiate Map
+    vis.map = L.map('station-map').setView(vis.mapPosition, 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(vis.map);
+
+    vis.wrangleData();
+};
+
+
+/*
+ *  Data wrangling
+ */
+
+StationMap.prototype.wrangleData = function() {
+    var vis = this;
+
+    // Currently no data wrangling/filtering needed
+    vis.displayData = vis.data;
+
+    // Update the visualization
+    vis.updateVis();
+
+};
+
+
+/*
+ *  The drawing function
+ */
+
+StationMap.prototype.updateVis = function() {
+    var vis = this;
+
+    // Add empty layer groups for the markers / map objects
+    vis.stationMarkers = L.layerGroup().addTo(vis.map);
+    vis.subwayStations = L.layerGroup().addTo(vis.map);
+
+    vis.displayData.forEach(function (d) {
+        // Create marker
+        //vis.mark = L.marker([d.lat,d.long]).bindPopup(d.name + "<br>" + d.nbBikes + " Bikes<br>" + d.nbEmptyDocks + " Empty Docks");
+        // Add marker to layer group
+        //vis.stationMarkers.addLayer(vis.mark);
+    });
+/*
+    // Decide Features
+    function styleMBTA(feature) {
+        return {color: feature.properties.LINE};
+    }
+
+    // Set Style to Function
+    L.geoJson(vis.MBTAData, {
+            style: styleMBTA
+        }
+    ).addTo(vis.map);
+*/
+};
