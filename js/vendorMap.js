c366b3b1b9a41303c2cb7e39b18d6c97b1792cdc
Luke Civantos <lfcivantos@gmail.com>
2017-11-11 15:51:38 -0500

    Push map without markers

--- js/vendorMap.js
+++ js/vendorMap.js
@@ -61,12 +61,12 @@ VendorMap.prototype.updateVis = function() {
     vis.stationMarkers = L.layerGroup().addTo(vis.map);
     vis.subwayStations = L.layerGroup().addTo(vis.map);
     console.log(vis.displayData.venues);
-    vis.displayData.venues.forEach(function (d) {
+    /*vis.displayData.venues.forEach(function (d) {
         // Create marker
         vis.mark = L.marker([d.lat,d.lon]).bindPopup(d.name + "<br>" + "Category: " + d.category);
         //Add marker to layer group
         vis.stationMarkers.addLayer(vis.mark);
-    });
+    });*/
 /*
     // Decide Features
     function styleMBTA(feature) {
